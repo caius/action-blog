c839bfd62e4d08f9d9bdb8db8b3a3b773597311b
Caius Durling <dev@caius.name>
2019-07-06 22:05:53 +0100

    Keep hugo install in $HOME

--- .github/action/hugo-build/entrypoint.sh
+++ .github/action/hugo-build/entrypoint.sh
@@ -2,9 +2,14 @@
 
 set -eu
 
-# Install tooling required
 HUGO_VERSION="$(awk '$1 ~ /gohugo/ { print $2 }' "${GITHUB_WORKSPACE}/.tool-versions")"
-wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
-tar zxf "hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
 
-./hugo
+# Install tooling required
+cd "${HOME}"
+wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
+tar zxf "hugo_${HUGO_VERSION}_Linux-64bit.tar.gz" hugo
+rm "hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
+
+# Build the site
+cd "${GITHUB_WORKSPACE}"
+$HOME/hugo
