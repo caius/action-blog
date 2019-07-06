2247d703e64788636aeb64d00427a78b9ceaa249
Caius Durling <dev@caius.name>
2019-07-06 22:50:38 +0100

    Update publish branch using git-worktree
    Saves copying things around, just tell git to checkout a different folder under a subpath

--- .github/action/gh-pages-publish/entrypoint.sh
+++ .github/action/gh-pages-publish/entrypoint.sh
@@ -9,20 +9,19 @@ set -eu
 #     git commit --allow-empty "Start of branch"
 #     git push
 
-publish_path="${GITHUB_WORKSPACE}/public"
-publish_branch="gh-pages"
+# FIXME: make these configured from ENV
+PUBLISH_FOLDER="public"
+PUBLISH_BRANCH="gh-pages"
 
 # Install dependencies
 apk update && apk add git
 
 # Publish!
-cd "${GITHUB_WORKSPACE}"
-git checkout "${publish_branch}"
+git worktree add --force "${PUBLISH_FOLDER}" "${PUBLISH_BRANCH}"
 
-# Copy the contents of $publish_path into pwd
-find public -mindepth 1 -exec cp -r {} . \;
-rm -r "${publish_path}"
-
-git add --all
-git -c user.name="ActionBot" -c user.email="actionbot@localhost" commit --message "Publish site from ${GITHUB_SHA}"
-git push origin "${publish_branch}"
+(
+  cd "${PUBLISH_FOLDER}"
+  git add --all
+  git -c user.name="ActionBot" -c user.email="actionbot@localhost" commit --message "Publish site from ${GITHUB_SHA}"
+  git push origin "${PUBLISH_BRANCH}"
+)
