5eda64c68b45535084d19e5b19e0510e38a13149
Caius Durling <dev@caius.name>
2019-07-06 22:19:27 +0100

    Grab submodules before doing owt else

--- .github/action/hugo-build/entrypoint.sh
+++ .github/action/hugo-build/entrypoint.sh
@@ -5,11 +5,17 @@ set -eu
 HUGO_VERSION="$(awk '$1 ~ /gohugo/ { print $2 }' "${GITHUB_WORKSPACE}/.tool-versions")"
 
 # Install tooling required
-cd "${HOME}"
-wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
-tar zxf "hugo_${HUGO_VERSION}_Linux-64bit.tar.gz" hugo
-rm "hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
+apk update && apk add git
+
+(
+  cd "${HOME}"
+  wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
+  tar zxf "hugo_${HUGO_VERSION}_Linux-64bit.tar.gz" hugo
+  rm "hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
+)
+
+# Make sure all submodules are grabbed
+git submodule update --init
 
 # Build the site
-cd "${GITHUB_WORKSPACE}"
 $HOME/hugo
